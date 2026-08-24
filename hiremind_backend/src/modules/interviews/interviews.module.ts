53886cd3d5cd6b68b80a37b7d4eb86c1e7718d6b
Hazem <hazem@HAZEM>
2026-08-24 16:21:43 +0100

    fix: resolve circular module dependency between InterviewsModule and CvModule in NestJS backend

--- hiremind_backend/src/modules/interviews/interviews.module.ts
+++ hiremind_backend/src/modules/interviews/interviews.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { InterviewsController } from './interviews.controller';
 import { AdaptiveInterviewService } from './adaptive-interview.service';
 import { AudioStreamingService } from './audio-streaming.service';
+import { CvModule } from '../cv/cv.module';
 
 @Module({
+  imports: [forwardRef(() => CvModule)],
   controllers: [InterviewsController],
   providers: [AdaptiveInterviewService, AudioStreamingService],
   exports: [AdaptiveInterviewService, AudioStreamingService],
